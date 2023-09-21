b788ae89ea7d5767d81203be818eb7c6ddebee47
Juliano Nobrega <48964299+JulianoNobrega@users.noreply.github.com>
2023-09-20 22:37:44 -0300

    Add files via upload

---
+++ program2.asm
@@ -0,0 +1,15 @@
+SECTION .data
+    decimal 			DB 100
+    hexadecinalPos 	DB 0AFh
+    hexadecimalPre 	DB 0xAF
+    binarioPos 			DB 10101010b
+    binarioPre 			DB 0b1010101011
+    octalPos 			DB 777o
+    octalPre 			DB 0o777
+
+SECTION .text
+GLOBAL main
+main:
+
+    MOV EAX,0
+    RET
