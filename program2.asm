28c4c419364567b8bf388a6a2d481c8db931165a
Juliano Nobrega <48964299+JulianoNobrega@users.noreply.github.com>
2023-09-22 12:25:47 -0300

    Update program2.asm

--- program2.asm
+++ program2.asm
@@ -1,11 +1,11 @@
 SECTION .data
     decimal 			DB 100
-    hexadecinalPos 	DB 0AFh
-    hexadecimalPre 	DB 0xAF
+    hexadecinalPos 	    DB 0AFh
+    hexadecimalPre 	    DB 0xAF
     binarioPos 			DB 10101010b
     binarioPre 			DB 0b1010101011
-    octalPos 			DB 777o
-    octalPre 			DB 0o777
+    octalPos 			DW 777o
+    octalPre 			DW 0o777
 
 SECTION .text
 GLOBAL main
