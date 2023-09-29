a009663ad21d13a21417e67a2849e898bcabbaf9
Juliano Nobrega <48964299+JulianoNobrega@users.noreply.github.com>
2023-09-29 00:10:48 -0300

    Add files via upload

--- program5.asm
+++ program5.asm
@@ -1,54 +1,63 @@
+; ===================================
 ; COMPARANDO VALORE DOS REGISTRADORES
+; ===================================
 
 ; Parte 1 - Variáveis DEFINIDAS na memória
 section .data
-    numero1 db 5   ; Primeiro número
-    numero2 db 6   ; Segundo número
+    numero1 dd '2' ; Preferencialmente entre 0 e 9
+    numero2 dd '3' ; Preferencialmente entre 0 e 9
     mensagem1 db "O maior número é: ", 0
     mensagem2 db "Os dois números são iguais!", 0
 
-; Parte 2 - Código do Programa
+; Parte 2 - Variável NÃO-DEFINIDA que vai guardar o maior número
+segment .bss
+    maior_numero resb 1 ; Espaço reservado na memória para a variável maior_numero
+    
+; Parte 3 - Código do Programa
 section .text
 global _start
 _start:
+    mov eax, [numero1] ; Carregando valor no registrador EAX
+    mov ebx, [numero2] ; Carregando valor no registrador EBX
+    cmp eax, ebx ; Instrução de COMPARAÇÃO dos registradores EAX e EBX
+    
+    je numeros_iguais ; CASO 1 (Jump If Equal) salta para outra seção "numeros iguais" e FINALIZA o programa
+    jg numero1_e_maior ; CASO 2 (Jumper If Greater), quando o primeiro numero é maior, salta para outra seção 
+    jb numero2_e_maior ; CASO 3 (Jumber If Less), quando o segundo numero é maior, salta para outra seção
 
-    ; Carregando os valores nos registradores
-    mov EAX, [numero1]
-    mov EBX, [numero2]
+numero2_e_maior: ; CASO 3
+    mov [maior_numero], ebx ; A variável não definida recebe o valor do EBX
+    jmp print
 
-    ; Comparando os valores
-    cmp EAX, EBX
-    je equal ; CASO 1: Se os valores são IGUAIS, salto para a seção "equal"
+numero1_e_maior: ; CASO 2
+    mov [maior_numero], eax ; A variável não definida recebe o valor do EAX
+    jmp print ; SALTO/JUMP para a seção Print
 
-    ; CASO 2: Se o primeiro número for MAIOR que o segundo, imprimir o primeiro número
-    jg numero1_e_maior ; JG = Jumper Greater
-    mov EAX, EBX ; Atribui o valor do registrador EBX no registrador EAX
-    jmp print ; Instrução "Jumper" vai para seção "Print"
+numeros_iguais: ; CASO 1
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, mensagem2
+    mov edx, 29 ; Comprimento da STRING
+    int 0x80
+    jmp end ; Finaliza o programa
 
-; Seção nova, quando o número 1 é maior.
-numero1_e_maior:
-    mov EAX, [numero1]
-    jmp print ; Instrução "Jumper" vai para a seção "print"
-
-; Seção chamada quando os dois valores são iguais
-equal:
-    mov EAX, 4
-    mov EBX, 1
-    mov ECX, mensagem2
-    mov EDX, 27
+print: ; PRINT da mensagem, do maior número
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, mensagem1
+    mov edx, 20 ; Comprimento da STRING
     int 0x80 ; Syscall para WRITE
-    jmp end ; Salto para a seção END
+    jmp print2 ; SALTO/JUMP para a seção Print2
 
-; Impressão do maior número
-print:
-    ; Imprimir o valor no EAX
-    mov EBX, EAX
-    mov EAX, 4 ; Código da Syscall
-    mov ECX, mensagem1 ; String da memória
-    mov EDX, 18 ; Comprimento da STRING
+print2: 
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, maior_numero
+    mov edx, 1 ; Comprimento da STRING
     int 0x80 ; Syscall para WRITE
+    jmp end ; Finaliza o programa
 
-end:
-    ; Finalizando o programa
-    mov EAX, 1 ; Atribuição do valor 1 no registrador ACUMULADOR
+end: ; Finaliza o programa
+    mov eax, 1
+    xor ebx, ebx 
     int 0x80 ; Syscall para EXIT
